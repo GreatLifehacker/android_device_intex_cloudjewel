4025b71d9d72ac9118268e0f942f7238b2380f71
GreatLifehacker <jesuisgowrishankar@aol.com>
2022-12-04 18:21:52 +0530

    Add BoardConfig32.mk since this device is arm32
    This file is just a copy of BoardConfig for now.
    
    Signed-off-by: GreatLifehacker <jesuisgowrishankar@aol.com>

---
+++ BoardConfig32.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/intex/cloudjewel
+
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+
+TARGET_BOARD_PLATFORM := mt6735m
+TARGET_BOOTLOADER_BOARD_NAME := cloudjewel
+TARGET_ARCH := arm
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+# BOARD_RECOVERYIMAGE_PARTITION_SIZE := 7733504
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_HAS_NO_REAL_SDCARD := false 
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_HAS_NO_MISC_PARTITION := false
+BOARD_RECOVERY_SWIPE := true
+BOARD_USES_MMCUTILS := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+TW_USE_TOOLBOX := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+include $(LOCAL_PATH)/kernel.mk
+include device/generic/twrpbuilder/BoardConfig32.mk
+
