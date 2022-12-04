62d68238ad82a2416501dcf2f854dababf79045d
GreatLifehacker <jesuisgowrishankar@aol.com>
2022-12-04 18:30:22 +0530

    Update BoardConfig, first successful boot!
    + Builds successfully against omni-5.1 TWRP minimal manifest
    + Modify BOARD_FLASH_BLOCK_SIZE (KERNEL_PAGESIZE * 64)
    + Add support for External SD
    + Choose default TWRP theme
    + Include twrp tools in build
    
    Signed-off-by: GreatLifehacker <jesuisgowrishankar@aol.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,20 +16,30 @@
 
 LOCAL_PATH := device/intex/cloudjewel
 
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+
 TARGET_BOARD_PLATFORM := mt6735m
 TARGET_BOOTLOADER_BOARD_NAME := cloudjewel
+TARGET_ARCH := arm
 
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
 # BOARD_RECOVERYIMAGE_PARTITION_SIZE := 7733504
-BOARD_FLASH_BLOCK_SIZE := 0
-BOARD_HAS_NO_REAL_SDCARD := true
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_HAS_NO_REAL_SDCARD := false
 BOARD_SUPPRESS_SECURE_ERASE := true
-BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_MISC_PARTITION := false
 BOARD_RECOVERY_SWIPE := true
 BOARD_USES_MMCUTILS := true
 BOARD_SUPPRESS_EMMC_WIPE := true
+TW_USE_TOOLBOX := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 include $(LOCAL_PATH)/kernel.mk
-include device/generic/twrpbuilder/BoardConfig32.mk
-
